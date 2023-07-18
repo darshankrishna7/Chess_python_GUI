da52b2773081b897042a07d037d41b12b9cd7d8b
darshankrishna7 <darshankrishna5a@gmail.com>
2023-07-18 15:48:51 +0800

    second commit

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
+import time
 from data.classes.Board import Board
 
 pygame.init()
 
 WINDOW_SIZE = (600,600)
+FONT = pygame.font.Font('freesansbold.ttf', 32)
 
 screen = pygame.display.set_mode(WINDOW_SIZE)
-
 board = Board(WINDOW_SIZE[0], WINDOW_SIZE[1])
+
 
 def draw(display):
     display.fill('white')
     board.draw(display)
     pygame.display.update()
+
+def show_text(message):
+    text = FONT.render(message, True, "Black", "White")
+    textRect = text.get_rect()
+    textRect.center = (WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] // 2)
+    screen.blit(text, textRect)
+    pygame.display.update()
+    time.sleep(3)
 
 if __name__ == '__main__':
     running = True
@@ -25,9 +35,9 @@ if __name__ == '__main__':
                 if event.button == 1:
                     board.handle_click(mx,my)
         if board.is_in_checkmate('black'):
-            print('White wins!!')
+            show_text("White Won!!")
             running = False
         elif board.is_in_checkmate('white'):
-            print('Black wins!!')
+            show_text("Black Won!!")
             running = False
         draw(screen)
